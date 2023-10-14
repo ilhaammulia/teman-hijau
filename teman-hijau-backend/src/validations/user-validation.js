3417f4ab40d0edad31f572259d12ee0881ddb110
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-10-14 16:04:38 +0700

    feat(validation): create user register validation

---
+++ teman-hijau-backend/src/validations/user-validation.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+const registerUserValidation = Joi.object({
+  username: Joi.string().max(20).required(),
+  password: Joi.string().max(100).required(),
+  role_id: Joi.string().max(10).required(),
+  first_name: Joi.string().max(100).required(),
+  last_name: Joi.string().max(100).required(),
+  email: Joi.string().max(100).required(),
+});
+
+export { registerUserValidation };
