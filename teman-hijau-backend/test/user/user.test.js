afbc5340886b2a1f5db2461f924ba95734dfecb8
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-10-15 20:16:49 +0700

    fix(middleware/verify-auth): save user data from decoded token

--- teman-hijau-backend/test/user/user.test.js
+++ teman-hijau-backend/test/user/user.test.js
@@ -50,4 +50,28 @@ describe("GET /api/users", () => {
     expect(result.status).toBe(401);
     expect(result.body.errors).toBeDefined();
   });
+
+  it("should reject if authorization is invalid", async () => {
+    const result = await supertest(web)
+      .get("/api/users")
+      .set("authorization", `Bearer token`);
+
+    expect(result.status).toBe(401);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("should can fetch their profile", async () => {
+    const login = await supertest(web).post("/api/users/login").send({
+      username: "ilham",
+      password: "rahasia",
+    });
+
+    const result = await supertest(web)
+      .get("/api/users")
+      .set("authorization", `Bearer ${login.body.data.access_token}`);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBeDefined();
+    expect(result.body.data.profile).toBeDefined();
+  });
 });
