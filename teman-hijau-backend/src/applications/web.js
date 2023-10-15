157cdf69ba448f5f6d66ec3194850a440cd44535
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-10-15 19:51:13 +0700

    feat(application/web): register private api to web

--- teman-hijau-backend/src/applications/web.js
+++ teman-hijau-backend/src/applications/web.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { errorMiddleware } from "../middlewares/error-middleware.js";
 import { publicRouter } from "../routes/public-api.js";
+import { privateRouter } from "../routes/private-api.js";
 
 export const web = express();
 web.use(express.json());
 
 web.use(publicRouter);
+web.use(privateRouter);
 
 web.use(errorMiddleware);
