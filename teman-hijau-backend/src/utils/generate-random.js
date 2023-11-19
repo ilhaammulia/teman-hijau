103c33bb188bf1f667f3b61708fdb69b151600d8
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-11-19 20:43:01 +0700

    fix(utils): change prefix schema

--- teman-hijau-backend/src/utils/generate-random.js
+++ teman-hijau-backend/src/utils/generate-random.js
@@ -1,7 +1,7 @@
 function generateRandomId(prefix) {
   const timestamp = new Date().getTime();
   const random = Math.floor(Math.random() * 1000 * timestamp);
-  return `${prefix}-${random}`;
+  return `${prefix}${random}`;
 }
 
 export { generateRandomId };
