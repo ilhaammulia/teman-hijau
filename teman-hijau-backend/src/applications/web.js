d7167893e1b70fd0d07d1b76b2465b242a4d1c9c
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-12-17 14:17:51 +0700

    feat(web): add cors

--- teman-hijau-backend/src/applications/web.js
+++ teman-hijau-backend/src/applications/web.js
@@ -3,7 +3,10 @@ import { errorMiddleware } from "../middlewares/error-middleware.js";
 import { publicRouter } from "../routes/public-api.js";
 import { privateRouter } from "../routes/private-api.js";
 
+import cors from "cors";
+
 export const web = express();
+web.use(cors());
 web.use(express.json());
 
 web.use(publicRouter);
